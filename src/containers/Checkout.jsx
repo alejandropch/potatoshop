0c91bdaeb7a5765970a24c8d7af35b5289bb9a17
alejandropch <alejandropachas1@gmail.com>
2021-10-26 16:41:05 -0500

    checkout basis implemented

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import '../styles/components/Checkout.css'
 
 export default function Checkout() {
     return (
-        <div>
-            Checkout
+        <div className="Checkout">
+            <div className="Checkout-content">
+                
+                <h3>Items list</h3>
+                <div className="Checkout-item">
+                    <div className="Checkout-element">
+                        <h4>Item: </h4><span>$5</span>
+                    </div>
+                    <button className="btn" type="button">Delete</button>
+                </div>
+                
+            </div>
+            <div className="Checkout-sidebar">
+                <h3>Total price: $5</h3>
+                <button type="button">Proceed to checkout</button>
+            </div>
         </div>
     )
 }
