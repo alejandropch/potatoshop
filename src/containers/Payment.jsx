e4ed4b1b7130490099cb0a8f2195a7b8b850455f
alejandropch <alejandropachas1@gmail.com>
2021-10-26 19:41:09 -0500

    payment & success containers improved

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -3,8 +3,14 @@ import '../styles/components/Payment.css'
 
 export default function Payment() {
     return (
-        <div>
-            Payment
+        <div className="Payment">
+            <div className="Payment-content">
+                <h3>Items list</h3>
+                <div className="Payment-button">
+                    btn
+                </div>
+            </div>
+
         </div>
     )
 }
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -4,7 +4,15 @@ import '../styles/components/Success.css'
 export default function Success() {
     return (
         <div>
-            Success
+            <div className="Success">
+                <div className="Success-content">
+                    <h2>Thanks for your purchase:)</h2>
+                    <span>Your item will arrive in 3 days to your address!</span>
+                    <div className="Success-map">
+                        google maps
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
