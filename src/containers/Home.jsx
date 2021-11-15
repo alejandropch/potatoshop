92764590bad95aef74f4e59c7d360a3e024be6cd
alejandropch <alejandropachas1@gmail.com>
2021-11-15 14:00:29 -0500

    using react-helmet

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import {Helmet} from 'react-helmet'
 import initialState from '../../initialState'
 import Products from '../components/Products'
 
 export default function Home() {
   return (
-
+    <>
+  <Helmet>
+    <title>Potato Shop - Products</title>
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:site" content="@alejantropper"/>
+    <meta name="twitter:creator" content="@alejantropper"/>
+    <meta name="twitter:title" content="Potato Shop - Products"/>
+    <meta name="twitter:description" content="Potato Shop - Products"/>
+    <meta property="og:title" content="Potato Shop - Products"/>
+    <meta property="og:description" content="Potato Shop - Products"/>
+    <meta property="og:url" content="potato-shop-331316.web.app" />
+    <meta property="og:site_name" content="Potato Shop - Products" />
+    <meta property="og:locale" content="en_EN" />
+    <meta property="og:type" content="article" />
+  </Helmet>
   <Products products={initialState.products} />
+  </>
+
   )
 }
