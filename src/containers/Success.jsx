f4c9ddec7d378b8133b2f74ffadd8f040f8ee3ab
alejandropch <alejandropachas1@gmail.com>
2021-11-15 11:42:08 -0500

    [add]firebase config

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -3,7 +3,7 @@ import AppContext  from '../context/AppContext'
 import Map from '../components/Map'
  import useGoogleAddress from '../hooks/useGoogleAddress'
 import '../styles/components/Success.css'
-
+  
 export default function Success() {
   const {state}=useContext(AppContext)
   const {buyer}=state
